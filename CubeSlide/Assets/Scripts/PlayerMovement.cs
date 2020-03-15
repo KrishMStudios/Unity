80da3adb2936dc51b2c964112da231bad1bcd785
Krish Malik <krish.malik@gmail.com>
2020-03-15 13:39:15 -0700

    Unity Cubeslide game

---
+++ CubeSlide/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 1000f;
+
+    public float forceApplied = 40f;
+
+    public void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+
+            rb.AddForce(forceApplied * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+
+        if (Input.GetKey("a"))
+        {
+
+            rb.AddForce(-forceApplied * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+    }
+}
